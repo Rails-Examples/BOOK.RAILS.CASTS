822347ed026b218225e2e8df2368ce65a5ecd06d
Alexandre Sampaio <allexonrails@user.live.fake>
2014-02-02 21:03:38 -0200

    CHAPTER 1

--- chapter-1/app/controllers/greeting_controller.rb
+++ chapter-1/app/controllers/greeting_controller.rb
@@ -1,5 +1,11 @@
 class GreetingController < ApplicationController
 	def index
+		@age = 25
 		@welcome_message = "Welcome to your first Rails application"
+
+		@table = {
+			"headings" => ['addend', 'addend', 'sum'],
+			"body" => [[1,1,2], [1,2,3], [1,3,4]]
+		}
 	end
 end
