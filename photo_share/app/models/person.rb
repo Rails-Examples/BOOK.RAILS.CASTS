55eec8168b25fd9ebd5a59c18b737405dcd30547
Alexandre Sampaio <allexonrails@live.fake.com>
2014-02-10 23:59:23 -0200

    adicionando exemplos de validações em rails

--- photo_share/app/models/person.rb
+++ photo_share/app/models/person.rb
@@ -5,5 +5,9 @@ class Person < ActiveRecord::Base
 								[:state, :state],
 								[:zip, :zip]
 							]
+
+	validates_length_of :name, :within => 8..128
+	validates_format_of :email,
+						:with => /^([^@\s] + )@((?: [-a-z0-9] + \.) + [a-z]{2, })$/i
 							
 end
